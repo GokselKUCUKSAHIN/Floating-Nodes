fc0af58d84190c4a42ba8c1ba4640538e05e05cb
Jellybeanci <gokselkucuksahin@gmail.com>
2020-06-02 19:39:05 +0300

    Main Fixed.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,73 @@
-public class Main
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Main extends Application
 {
+    public static ObservableList<Node> child;
+    //
+    private static final String title = "JellyBeanci";
+    public static final int width = 1200;
+    public static final int height = 800;
+    private static Color backcolor = Color.rgb(51, 51, 51);
+
+    private static Timeline update;
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Pane root = new Pane();
+        child = root.getChildren();
+        //
+        root.setOnKeyPressed(e -> {
+            switch (e.getCode())
+            {
+                case F1:
+                {
+                    //PLAY
+                    update.play();
+                    break;
+                }
+                case F2:
+                {
+                    //PAUSE
+                    update.pause();
+                    break;
+                }
+                case F3:
+                {
+                    //Show Child Count
+                    System.out.println("Child Count: " + child.size());
+                    break;
+                }
+            }
+        });
+        update = new Timeline(new KeyFrame(Duration.millis(16), e -> {
+            //60 fps
+            System.out.println("loop test");
+
+        }));
+        update.setCycleCount(Timeline.INDEFINITE);
+        update.setRate(1);
+        update.setAutoReverse(false);
+        //update.play(); //uncomment for play when start
+        //
+        stage.setTitle(title);
+        stage.setResizable(false);
+        stage.setScene(new Scene(root, width - 10, height - 10,backcolor));
+        stage.show();
+        root.requestFocus();
+    }
 
     public static void main(String[] args)
     {
-        System.out.println("Hello, Wolrd!");
+        launch(args);
     }
 }
